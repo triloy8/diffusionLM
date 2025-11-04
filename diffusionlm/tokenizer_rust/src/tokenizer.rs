d4e4d85349a01945890f56dc9a3ad83b07fdb4e9
triloy8 <196841245+triloy8@users.noreply.github.com>
2025-11-04 23:15:05 +0100

    wip: from_files in rust bpe

--- diffusionlm/tokenizer_rust/src/tokenizer.rs
+++ diffusionlm/tokenizer_rust/src/tokenizer.rs
@@ -1,29 +1,84 @@
-use pyo3::prelude::*;
+// use pyo3::prelude::*;
 
-#[pyclass]
+use std::path::Path;
+use std::collections::HashMap;
+
+// #[pyclass]
 pub struct Tokenizer;
 
-#[pymethods]
+// #[pymethods]
 impl Tokenizer {
-    #[new]
-    fn new() -> Self {
+    // #[new]
+    pub fn new() -> Self {
         Tokenizer
     }
 
-    #[staticmethod]
-    pub fn from_files() -> Self{
-        todo!()
+    // #[staticmethod]
+    pub fn from_files<P: AsRef<Path>>(vocab_filepath: P, merges_filepath: P, special_tokens: Vec<String>) ->  Result<(), std::io::Error> {
+        let encoder: HashMap<u32, char> = gpt2_bytes_to_unicode();
+        let decoder: HashMap<char, u32> = encoder.iter().map(|(&id, &ch)| (ch, id)).collect();
+        // for (key, value) in &encoder {
+        //     println!("{key} => {value}");
+        // }
+
+        // vocab
+        let raw_gpt2_vocab = std::fs::read_to_string(vocab_filepath)?;
+        // println!("{raw_gpt2_vocab}");
+        let mut gpt2_vocab: HashMap<String, u32> = serde_json::from_str::<HashMap<String, u32>>(&raw_gpt2_vocab)?;
+        for special_token in special_tokens {
+            gpt2_vocab.insert(special_token, gpt2_vocab.values().len() as u32);
+        }
+        let vocab: HashMap<u32, &[u8]> = gpt2_vocab
+        .iter()
+        .map(|(word, &id)| (id, word.as_bytes())).collect();
+        
+        // if let Err(gpt2_vocab) = serde_json::from_str::<HashMap<String, u32>>(&raw_gpt2_vocab){
+        //     println!{"{gpt2_vocab}"};
+        // }
+        // for (key, value) in &gpt2_vocab {
+        //     println!("{key} => {value}");
+        // }
+        // for (key, &value) in &vocab {
+        //     println!("{key} => {:?}", &value);
+        // }
+
+        // merges
+        let gpt2_merges = std::fs::read_to_string(merges_filepath)?;
+        // println!("{gpt2_merges}");
+        
+
+        Ok(())
     }
 
     pub fn encode(&self) {
         todo!()
     }
 
-    pub fn encode_iteralble(&self) {
+    pub fn encode_iterable(&self) {
         todo!()
     }
 
     pub fn decode(&self) {
         todo!()
     }
+}
+
+fn gpt2_bytes_to_unicode() -> HashMap<u32, char> {
+    let allowed: std::iter::Chain<std::iter::Chain<std::ops::RangeInclusive<char>, std::ops::RangeInclusive<char>>, std::ops::RangeInclusive<char>> = ('!'..='~')
+        .chain('¡'..='¬')
+        .chain('®'..='ÿ');
+
+    let mut chars: Vec<char> = allowed.clone().collect();
+    let mut codes: Vec<u32> = allowed.map(|c| c as u32).collect();
+    let mut n = 0;
+
+    for i in 0..256{
+        if !codes.contains(&i){
+            codes.push(i);
+            chars.push(char::from_u32(256 + n).unwrap());
+            n += 1;
+        }
+    }
+
+    codes.into_iter().zip(chars).collect()
 }
--- diffusionlm/tokenizer_rust/src/tokenizer_cli.rs
+++ diffusionlm/tokenizer_rust/src/tokenizer_cli.rs
@@ -1,5 +1,12 @@
 #![cfg(feature = "cli")] 
+use std::println;
+
+use tokenizer_rs::tokenizer::Tokenizer;
 
 fn main() { 
-    println!("test");
+    let tokenizer = Tokenizer::new();
+    let vocab_filepath = "../../data/gpt2_vocab.json";
+    let merges_filepath = "../../data/gpt2_merges.txt";
+    let special_tokens = vec!["<|endoftext|>".to_string(), "<|mask|>".to_string()];
+    Tokenizer::from_files(vocab_filepath, merges_filepath, special_tokens);
  }
